051aab6662db0a32814aec804710a8083aebb59e
Ruteski <lruteski@gmail.com>
2022-05-12 09:35:27 -0300

    implementado pulo e animacoes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 8f;
+    [SerializeField] private float _jumpForce = 15f;
 
     private Rigidbody2D _rig;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+    private bool _isGrounded = true;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +34,19 @@ public class Player : MonoBehaviour
             _rig.velocity = new Vector2(_speed, _rig.velocity.y);
             _animator.SetBool("isWalking", true);
             _spriteRenderer.flipX = false;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) && _isGrounded) {
+            _rig.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _isGrounded = false;
+            _animator.SetTrigger("isJumping");
+            _animator.SetBool("isWalking", false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 6) {
+            _isGrounded = true;
         }
     }
 }
